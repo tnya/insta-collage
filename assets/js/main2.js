bb4eb3b07a216aafb4669c5a6db69223df2c2cff
Tanya Ramirez <tcramire@gmail.com>
2017-06-18 20:26:00 -0400

    js no funciona

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,17 +1,17 @@
-function drag(event){
-	event.dataTransfer.setData("text", event.target.id); /* setData le digo q tipo soy */ /*text pq los navegadores leen las imagenes como tipo texto */
-	/* sabe q el drag esta dentro de un id, ese es el objetivo para agarrar (formato, el objetivo) nuestro target es el objetivo*/
-	/* sabe que id usar pq tiene un drag*/
-
+//se especifica tipo de datos a transferir
+function drag(event) {
+  console.log(event.target.src);
+  event.dataTransfer.setData('text', event.target.id);
 }
 
-function finalDrop(hijo){
-	hijo.preventDefault();/*SIEMPRE va esto para que el navegador no lo manipule*/
+
+function permitirDrop(event){
+  event.preventDefault();
 }
 
-function drop(event){
-	event.preventDefault();
-
-	var dato=event.dataTransfer.getData("text");
-	event.target.appendChild(document.getElementById(dato)); 
+function drop(ev) {
+  event.preventDefault();
+  var photo_id = event.dataTransfer.getData('text');
+  event.target.appendChild(document.getElementById(photo_id));
 }
+
